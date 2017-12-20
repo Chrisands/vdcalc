2433f717f0012432428181ad03964f903a9cfdd5
Andrew <a.osijuk@gmail.com>
2017-12-21 00:00:42 +0300

    Added currentKey

--- src/Array.js
+++ src/Array.js
@@ -4,11 +4,44 @@ import Form from './Form'
 class Array extends Component {
   constructor(props) {
     super(props)
-    this.state = { count: 1, form: [{ cost: '', amount: '1', key: 1 }] }
+    this.state = {
+      count: 1,
+      currentKey: '',
+      form: [{ cost: '', amount: '1', key: 1 }]
+    }
 
     this.handleItemAdd = this.handleItemAdd.bind(this)
     this.handleItemDelete = this.handleItemDelete.bind(this)
+    this.handleCostChange = this.handleCostChange.bind(this)
+    this.handleAmountChange = this.handleAmountChange.bind(this)
+    this.currentKey = this.currentKey.bind(this)
   }
+
+  currentKey(e) {
+    this.setState({ currentKey: e })
+  }
+
+  handleCostChange(e) {
+    this.setState(prevState => ({
+      prevState,
+      form: [
+        {
+          cost: e,
+          amount: prevState.form[this.state.currentKey - 1].amount,
+          // product: prevState.form[this.state.currentKey].product,
+          key: prevState.form[this.state.currentKey - 1].key
+        }
+      ]
+    }))
+  }
+
+  handleAmountChange(e) {
+    this.setState(prevState => ({
+      form: [this.state.currentKey]
+    }))
+  }
+
+  handleProductChange() {}
 
   handleItemAdd(e) {
     const counter = this.state.count + 1
@@ -30,14 +63,19 @@ class Array extends Component {
     }
   }
 
-  handleChange(e) {
-    console.log(e.target.value)
-  }
-
   render() {
+    // const entries = this.state.form
+    // const costValue = this.state.form.cost
+    // const amountValue = this.state.form.amount
+
     return (
       <div>
-        <Form entries={this.state.form} onChange={this.handleChange} />
+        <Form
+          entries={this.state.form}
+          getKey={this.currentKey}
+          onCostValueChange={this.handleCostChange}
+          onAmountValueChange={this.handleAmountChange}
+        />
         <div>
           <button type="button" onClick={this.handleItemAdd}>
             +
--- src/Form.js
+++ src/Form.js
@@ -3,44 +3,46 @@ import React, { Component } from 'react'
 class Form extends Component {
   constructor(props) {
     super(props)
-    this.state = { cost: '', amount: '', product: '' }
 
     this.createForm = this.createForm.bind(this)
-
-    // this.handleChangeCost = this.handleChangeCost.bind(this)
-    // this.handleChangeAmount = this.handleChangeAmount.bind(this)
+    this.onCostChange = this.onCostChange.bind(this)
+    this.onAmountChange = this.onAmountChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  // handleChangeCost(e) {
-  //   // e = event
-  //   this.setState({ cost: e.target.value })
-  //   console.log(e.target.value)
-  // }
+  onCostChange(e) {
+    // e = event
+    this.props.onCostValueChange(e.target.value)
+  }
 
-  // handleChangeAmount(e) {
-  //   this.setState({
-  //     form: {
-  //       amount: e.target.value
-  //     }
-  //   })
-  // }
+  onAmountChange(e) {
+    // e = event
+    this.props.onAmountValueChange(e.target.value)
+  }
+
+  handleClick(e) {
+    this.props.getKey(e.target.getAttribute('id'))
+    // console.log(e.target.getAttribute('id'))
+  }
 
   createForm(item) {
     return (
-      <li key={item.key}>
+      <li onInput={this.handleClick} onClick={this.handleClick} key={item.key}>
         <form>
           <label>
             <input
               type="number"
-              onChange={this.handleChangeCost}
-              defaultValue={item.cost}
+              id={item.key}
+              value={item.cost}
+              onChange={this.onCostChange}
             />
           </label>
           <label>
             <input
               type="number"
-              onChange={console.log(item.amount)}
-              defaultValue={item.amount}
+              id={item.key}
+              value={item.amount}
+              onChange={this.onAmountChange}
             />
           </label>
           <label>{item.amount}</label>
