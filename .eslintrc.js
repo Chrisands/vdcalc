ff03b5925c10d769134b6b984cd2685c5cfb3e69
Andrew <a.osijuk@gmail.com>
2017-12-24 13:56:19 +0300

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'no-lone-blocks': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+  }
+}
