fc39e493d66361b5c0ccf74c8553927cece774f6
Andrey Osijuk <a.osijuk@gmail.com>
2019-02-16 19:22:12 +0300

    feat(dashboard): added header
    Signed-off-by: Andrey Osijuk <a.osijuk@gmail.com>

---
+++ packages/dashboard/src/components/desktop/Header.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { injectIntl } from 'react-intl'
+import { Row, Layout } from 'flex-layouts'
+import { Block } from '@vd/ui/src/content'
+import { Logo } from '@vd/ui/src/logo'
+import { Select } from '@vd/ui/src/select'
+import { Ghost } from '@vd/ui/src/button'
+import { CopyIcon, MinusIcon, PlusIcon } from '@vd/ui/src/icons'
+
+const Header = () => (
+  <Block>
+    <Row
+      justify='center'
+    >
+      <Layout basis='800px'>
+        <Row
+          align='center'
+        >
+          <Layout>
+            <Logo
+              width={40}
+              height={40}
+            />
+          </Layout>
+          <Layout basis='16px' />
+          <Layout basis='152px'>
+            <Select
+              value='Calculator'
+            />
+          </Layout>
+          <Layout grow={1} />
+          <Layout>
+            <Ghost
+              width='medium'
+              height='medium'
+              color='gray600'
+            >
+              <PlusIcon
+                color='gray400'
+              />
+            </Ghost>
+          </Layout>
+          <Layout basis='8px' />
+          <Layout>
+            <Ghost
+              width='medium'
+              height='medium'
+              color='gray600'
+            >
+              <MinusIcon
+                color='gray400'
+              />
+            </Ghost>
+          </Layout>
+          <Layout basis='248px' />
+          <Layout>
+            <Ghost
+              width='medium'
+              height='medium'
+              color='gray600'
+            >
+              <CopyIcon
+                width={10}
+                height={12}
+                color='gray400'
+              />
+            </Ghost>
+          </Layout>
+        </Row>
+      </Layout>
+    </Row>
+  </Block>
+)
+
+export default injectIntl(Header)
