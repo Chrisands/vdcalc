283f06abd0790dd6e338cba87bee33e79aeb23e1
Andrey Osijuk <a.osijuk@gmail.com>
2019-02-16 17:01:14 +0300

    fix(dashboard): removed config reducer
    what's new:
    - removed config reducer
    - corrected App
    
    Signed-off-by: Andrey Osijuk <a.osijuk@gmail.com>

--- packages/dashboard/src/reducers/index.js
+++ packages/dashboard/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
-import config from './config'
 import locale from './locale'
 
 export default history => combineReducers({
   router: connectRouter(history),
-  config,
   locale,
 })
