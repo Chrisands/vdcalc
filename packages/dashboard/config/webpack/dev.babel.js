d5914b28267e77adb08007f98cd866e822db9000
Andrey Osijuk <a.osijuk@gmail.com>
2019-02-21 23:12:29 +0300

    feat(env): upgraded dependencies
    what's new:
    - upgraded dependencies
    - fixed webpack config
    - added fonts
    
    Signed-off-by: Andrey Osijuk <a.osijuk@gmail.com>

--- packages/dashboard/config/webpack/dev.babel.js
+++ packages/dashboard/config/webpack/dev.babel.js
@@ -129,6 +129,7 @@ export const plugins = [
   new HTMLWebpackPlugin(({
     title: 'Vegan Delivery',
     inject: false,
+    mobile: true,
     template: htmlTemplate,
     appMountId: 'app',
   })),
